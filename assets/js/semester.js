ca0c54640fc29ab6e10a56fdb627b82104bb7284
Marc Rosenberger <marc.rosenberger@iem.thm.de>
2021-06-04 14:51:51 +0200

    UPDATE: Sun Caption

--- assets/js/semester.js
+++ assets/js/semester.js
@@ -4,7 +4,7 @@ var clientWidth;
 var clientHeight;
 
 //System Settings
-let semester = 1;
+let semester = 3;
 var planetSpacing = 5;
 var borderSpace = 5;
 var aspectRatio = 0.8; //Compress Orbit y-Size by this Value
@@ -53,6 +53,15 @@ function displayOrbit(){
                 sun.setAttribute("cx", clientWidth/2);
                 sun.setAttribute("cy", clientHeight/2);        
                 svg.appendChild(sun);
+
+                let sunCaption = document.createElementNS("http://www.w3.org/2000/svg", "text");
+                sunCaption.id = "semNumber";
+                sunCaption.textContent = arabicToRoman(semester);
+                sunCaption.style.textAnchor = "middle";
+                sunCaption.style.fontSize = sunSize*0.8; //*0.8 to try and keep Text inside Sun
+                sunCaption.setAttribute("x", clientWidth/2);
+                sunCaption.setAttribute("y", clientHeight/2+sunSize/3.5); //sunSize/3.5 to try and center   
+                svg.appendChild(sunCaption);
 
                 var viewportAspect = clientHeight/clientWidth;
                 
@@ -105,6 +114,8 @@ function displayOrbit(){
             }
         });
     });
+
+    gsap.ticker.add(animateOrbits);
 }
 
 
@@ -130,4 +141,27 @@ function animateOrbits(){
     }
 }
 
-gsap.ticker.add(animateOrbits);
+function arabicToRoman(arabic){
+    switch(arabic){
+        case 1:
+            return "I";
+        case 2:
+            return "II";
+        case 3:
+            return "III";
+        case 4:
+            return "IV";
+        case 5:
+            return "V";
+        case 6:
+            return "VI";
+        case 7:
+            return "VII";
+        case 8:
+            return "VIII";
+        case 7:
+            return "IX";
+        case 8:
+            return "X";
+    }
+}
