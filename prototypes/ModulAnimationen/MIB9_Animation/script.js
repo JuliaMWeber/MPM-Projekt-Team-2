f20150a9a892b856f793f9a2c3efcc860ded6a9c
Antony Zwickl <antony.zwickl@iem.thm.de>
2021-06-19 06:57:01 +0000

    Deleted prototypes/ModulAnimationen/MIB9_Animation/svg/.gitkeep, prototypes/ModulAnimationen/MIB9_Animation/svg/MIB9.svg, prototypes/ModulAnimationen/MIB9_Animation/reset.css, prototypes/ModulAnimationen/MIB9_Animation/index.html, prototypes/ModulAnimationen/MIB9_Animation/script.js, prototypes/ModulAnimationen/MIB9_Animation/styles.css files

--- prototypes/ModulAnimationen/MIB9_Animation/script.js
+++
@@ -1,7 +0,0 @@
-// Hier kommt das Custom Script Zeug rein
-//testanimation mib9
-let mib9Object = document.getElementById("mib9");
-let thmColorless = mib9Object.contentDocument.getElementById("THMungefaerbt");
-let thmColorfull = mib9Object.contentDocument.getElementById("THMgefaerbt");
-gsap.from(thmColorless, {opacity: 0, delay: 1.5});
-gsap.from(thmColorfull, {opacity: 0, duration: 3, delay: 3});
