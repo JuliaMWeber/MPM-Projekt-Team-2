af97bfc97e0b5e4ab5f74edd3fa648645b2346fd
Antony Zwickl <antony.zwickl@iem.thm.de>
2021-06-21 19:47:26 +0000

    Update prototypes/ModulAnimationen/MIB9_Animation/script.js

--- prototypes/ModulAnimationen/MIB9_Animation/script.js
+++ prototypes/ModulAnimationen/MIB9_Animation/script.js
@@ -1,16 +1,17 @@
-// Hier kommt das Custom Script Zeug rein
-//testanimation mib9
-
 $('#mib9').on('load', () => {
     console.log("SVG Loaded");
   let mib9Object = document.getElementById("mib9");
     let thmColored = mib9Object.contentDocument.getElementById("thm-colored");
-  
-    gsap.from(thmColored, {opacity: 0, duration: 3, delay: 3});
-    let vivus = new Vivus('mib9', {
-      type: "scenario",
-    }, function(e) {
-      console.log("Animation done");
-    }); 
-    vivus.play();
+  let tl = gsap.timeline({
+    repeat: -1
   });
+    tl.from(thmColored, {opacity: 0, duration: 3, delay: 3});
+    let vivus = new Vivus('mib9', {
+      type: "scenario", duration: 330
+    }, function(e) {  
+      vivus.reset().play();
+      console.log("Animation done");    
+    });
+
+  });
+
