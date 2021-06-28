bb36732f160089000d863278a5e31b6797995202
Omer Khaber <omerkhaber@Omers-MacBook-Pro.local>
2021-06-28 13:11:53 +0200

    QRCode Mini-Anpassung

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,12 +71,12 @@ function switchToSemester(semester) {
 // QR-Code
 function generateQRCode() {
   let qrcodewrapper = $('#app').find('#satellite');
-  if(qrcodewrapper.length > 0) {
+  if (qrcodewrapper.length > 0) {
 
     let sat = qrcodewrapper.find('#satellite_svg').first();
-    
+
     $(sat).on("load", function () {
-      if(sat[0].contentDocument) {
+      if (sat[0].contentDocument) {
         let wing = $(sat[0].contentDocument).find('#qrplace')[0];
 
         let url = window.location.href;
@@ -84,9 +84,9 @@ function generateQRCode() {
           text: url,
           width: 250,
           height: 250,
-          colorDark : "#000000",
-          colorLight : "transparent",
-          correctLevel : QRCode.CorrectLevel.H,
+          colorDark: "#000000",
+          colorLight: "transparent",
+          correctLevel: QRCode.CorrectLevel.H,
           useSVG: true
         });
 
@@ -99,16 +99,28 @@ function generateQRCode() {
         });
 
         let wiggle = gsap.timeline();
+        // wiggle.to(sat, {
+        //   x: 15,
+        //   y: 15,
+        //   rotate: 5,
+        //   repeat: -1,
+        //   yoyo: true,
+        //   duration: 2,
+        // });
+
         wiggle.to(sat, {
-          x: 15,
-          y: 15,
-          rotate: 5,
+          x: 'random(-15, 15, 5)',
+          y: 'random(-15, 15, 5)',
           repeat: -1,
           yoyo: true,
-          duration: 2,
+          repeatRefresh: true
+        })
+
+        let comein = gsap.timeline({
+          duration: 0.5,
+          paused: true
         });
 
-        let comein = gsap.timeline({duration: 0.5, paused: true});
         let scale = 5;
         comein.to(sat.parent(), {
           scale: scale,
@@ -117,19 +129,21 @@ function generateQRCode() {
           y: -(device.height / 2) + (sat.parent().height() * scale / 2),
           transformOrigin: "bottom",
         });
-        sat.parent().on('click', function() {
+
+        sat.parent().on('click', function () {
 
           console.log((device.width / 2), (sat.parent().width() * scale / 2));
 
-          if( sat.parent().attr('clicked') == 'true') {
+          if (sat.parent().attr('clicked') == 'true') {
             comein.reverse();
             wiggle.play();
 
             sat.parent().attr('clicked', false);
+
           } else {
             comein.play();
             wiggle.pause();
-        
+
             sat.parent().attr('clicked', true);
           }
         });
