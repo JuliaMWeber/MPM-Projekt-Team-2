8baa536dfdee0a0ef356a589af44b0196410c2aa
Omer Khaber <omerkhaber@Omers-MacBook-Pro.local>
2021-06-29 14:33:42 +0200

    MIB2-Anpassung

--- prototypes/ModulAnimationen/MIB2_Animation/app.js
+++ prototypes/ModulAnimationen/MIB2_Animation/app.js
@@ -1,92 +1,139 @@
-// gsap.from('.laptop', {
-//     duration: 3,
-//     x: 300,
-//     opacity: 0,
-//     scale: 0.5,
-//     ease: 'power2.inOut'
-// })
+// let svgObj = document.getElementById("MIB2Obj");
 
-// gsap.to('.laptop', {
-//     duration: 2.5,
-//     x: -150,
-//     ease: "elastic.out(1, 0.3)"
-// })
+// svgObj.onload = function () {
+//     content = svgObj.contentDocument;
+//     // let firstBtn = document.getElementById("firstBtn");
+//     // let secondBtn = document.getElementById("secondBtn");
+//     // let thirdBtn = document.getElementById("thirdBtn");
+//     // let firstWave = document.getElementById("firstWave");
+//     // let secondWave = document.getElementById("secondWave");
+//     // let thirdWave = document.getElementById("thirdWave");
+//     // let speakerWave = document.getElementById("speakerWave");
 
-// gsap.to('.laptop', {
-//     duration: 2,
-//     x: -200,
-//     ease: "bounce"
-// })
+//     let masterTl = gsap.timeline({
+//         repeat: -1,
+//         repeatDelay: 1
+//     });
 
-// gsap.from('.laptop', {
-//     duration: 3,
-//     y: -300,
-//     opacity: 0,
-//     scale: 0.5,
-//     stagger: 0.6
-// });
-
-// gsap.from(".laptop", 1, {
-//     scale: 0.1,
-//     y: -60,
-//     yoyo: true,
-//     opacity: 0,
-//     ease: "power1.inOut",
-//     delay: 1,
-//     stagger: 1.5
-// })
+//     masterTl.to(firstBtn, {
+//         y: -10
+//     }, '+=0.2').to(secondBtn, {
+//         y: -50
+//     }, '+=0.2').to(thirdBtn, {
+//         y: -90
+//     }, '+=0.2')
 
 
-let masterTl = gsap.timeline({
-    repeat: -1,
-    repeatDelay: 1
-});
+//     gsap.to(firstWave, {
+//         duration: 2,
+//         scale: 0.5,
+//         repeat: -1
+//     })
 
-// masterTl.to('.cls-9', {
-//     y: -140
-// }, '+=0.9')
+//     gsap.to(secondWave, {
+//         duration: 1,
+//         scale: 0.5,
+//         repeat: -1
+//     })
 
-masterTl.to('#firstBtn', {
-    y: -10
-}, '+=0.2').to('#secondBtn', {
-    y: -50
-}, '+=0.2').to('#thirdBtn', {
-    y: -90
-}, '+=0.2')
+//     gsap.to(thirdWave, {
+//         duration: 0.5,
+//         scale: 0.9,
+//         repeat: -1
+//     })
 
-// let tl = gsap.timeline({
+//     gsap.from(speakerWave, {
+//         duration: 1,
+//         scale: 1.1,
+//         repeat: -1
+//     })
+
+// }
+
+// let masterTl = gsap.timeline({
 //     repeat: -1,
 //     repeatDelay: 1
 // });
 
-// tl.to('.cls-11', {
-//     x: -165
-// }, '+=0.5').to('.cls-12', {
-//     x: -165
-// }, '+=0.5').to('.cls-13', {
-//     x: -165
-// }, '+=0.5')
+// masterTl.to('#firstBtn', {
+//     y: -10
+// }, '+=0.2').to('#secondBtn', {
+//     y: -50
+// }, '+=0.2').to('#thirdBtn', {
+//     y: -90
+// }, '+=0.2')
 
-gsap.to('#firstWave', {
-    duration: 2,
-    scale: 0.5,
-    repeat: -1
-})
 
-gsap.to('#secondWave', {
-    duration: 1,
-    scale: 0.5,
-    repeat: -1
-})
+// gsap.to('#firstWave', {
+//     duration: 2,
+//     scale: 0.5,
+//     repeat: -1
+// })
 
-gsap.to('#thirdWave', {
-    duration: 0.5,
-    scale: 0.9,
-    repeat: -1
-})
+// gsap.to('#secondWave', {
+//     duration: 1,
+//     scale: 0.5,
+//     repeat: -1
+// })
 
-gsap.from('#speakerWave', {
-    duration: 1,
-    scale: 1.1,
-    repeat: -1
-})
+// gsap.to('#thirdWave', {
+//     duration: 0.5,
+//     scale: 0.9,
+//     repeat: -1
+// })
+
+// gsap.from('#speakerWave', {
+//     duration: 1,
+//     scale: 1.1,
+//     repeat: -1
+// })
+
+window.addEventListener("load", function () {
+    var svgObject = document.getElementById('MIB2Obj').contentDocument;
+    let firstBtn = svgObject.getElementsByClassName("firstBtn");
+    let secondBtn = svgObject.getElementsByClassName("secondBtn");
+    let thirdBtn = svgObject.getElementsByClassName("thirdBtn");
+    let firstWave = svgObject.getElementsByClassName("firstWave");
+    let secondWave = svgObject.getElementsByClassName("secondWave");
+    let thirdWave = svgObject.getElementsByClassName("thirdWave");
+    let speakerWave = svgObject.getElementsByClassName("speakerWave");
+
+    let masterTl = gsap.timeline({
+        repeat: -1,
+        repeatDelay: 1
+    });
+
+    masterTl.to(firstBtn, {
+        y: -10
+    }, '+=0.2').to(secondBtn, {
+        y: -50
+    }, '+=0.2').to(thirdBtn, {
+        y: -90
+    }, '+=0.2')
+
+
+    gsap.to(firstWave, {
+        duration: 2,
+        scale: 0.5,
+        repeat: -1
+    })
+
+    gsap.to(secondWave, {
+        duration: 1,
+        scale: 0.5,
+        repeat: -1
+    })
+
+    gsap.to(thirdWave, {
+        duration: 0.5,
+        scale: 0.9,
+        repeat: -1
+    })
+
+    gsap.from(speakerWave, {
+        duration: 1,
+        scale: 1.1,
+        repeat: -1
+    })
+
+});
