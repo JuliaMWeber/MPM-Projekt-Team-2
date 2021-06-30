007d7542cea4cffa4c6d9aaa66c0bcedcfe4e9f5
Omer Khaber <omerkhaber@Omers-MacBook-Pro.local>
2021-06-30 13:36:38 +0200

    MIB3-Anpassung

--- prototypes/ModulAnimationen/MIB3_Animation/app.js
+++ prototypes/ModulAnimationen/MIB3_Animation/app.js
@@ -1,88 +1,108 @@
-var tl = gsap.timeline({
-    defaults: {
-        duration: 0.5,
-        ease: 'none',
-        opacity: 0
-    }
-});
+gsap.registerPlugin(TextPlugin, EasePack);
 
-var tl1 = gsap.timeline({
-    defaults: {
-        duration: 0.1,
-        ease: 'power3.out',
+window.addEventListener('load', function () {
+    let svgObj = document.getElementById('MIB3Obj').contentDocument;
+    let cls3 = svgObj.querySelectorAll('.cls-3, .cls-18');
+    let cls21 = svgObj.getElementsByClassName('cls-21');
+    let cls22 = svgObj.getElementsByClassName('cls-22');
+    let cls23 = svgObj.getElementsByClassName('cls-23');
+    let cls26 = svgObj.getElementsByClassName('cls-26');
+    let cls27 = svgObj.getElementsByClassName('cls-27');
+    let cls28 = svgObj.getElementsByClassName('cls-28');
+    let cursor = svgObj.getElementById('cursor');
+    let title = svgObj.querySelectorAll('.title');
+
+    let c = gsap.to(cursor, {
         opacity: 0,
-        stagger: 0.03
-    }
-});
+        ease: 'power2.inOut',
+        repeat: -1
+    });
 
-tl1.from('#cls3', {
-    x: 100,
-}, '+=0.01')
+    const words = ['Designer.', 'Entwickler.', 'Entrepreneur.'];
 
-// .from('.cls-18', {
-//     x: 400,
+    let masterTL = gsap
+        .timeline({
+            repeat: -1
+        })
+        .pause();
 
-// }, '+=1').from('.cls-19', {
-//     x: 400,
+    words.forEach((word) => {
+        let tl = gsap.timeline({
+            repeat: 1,
+            yoyo: true,
+            repeatDelay: 1
+        });
+        tl.to(title, {
+            duration: 1,
+            text: word
+        });
+        masterTL.add(tl);
+    });
 
-// }, '+=1')
-
-
-// tl.from('.cls-28', {
-//     y: 7 vw,
-//     stagger: 0.5,
-//     ease: 'power3.out'
-// }, '+=0.2').from('.cls-32', {
-//     y: 400,
-// }, '+=0.2')
-
-tl.from('.cls-21', {
-    y: -400,
-    ease: 'power3.out'
-}, '+=0.2').from('.cls-26', {
-    x: -400
-}, '-=0.2').from('.cls-27', {
-    x: -400,
-    stagger: 0.2
-}, '+=0.3').from('.cls-22', {
-    y: 100
-}, '-=0.2').from('.cls-23', {
-    x: -400,
-    stagger: 0.2
-}, '-=0.2')
-
-
-
-tl.from('.cls-28', {
-    duration: 0.5,
-    stagger: 0.2,
-    x: "-100vw",
-    ease: "power3.out",
-    onComplete: () => masterTL.play()
-})
-
-const words = ['Designer.', 'Entwickler.', 'Entrepreneur.'];
-
-let cursor = gsap.to('#cursor', {
-    opacity: 0,
-    ease: 'power2.inOut',
-    repeat: -1
-});
-
-let masterTL = gsap.timeline({
-    repeat: -1,
-
-}).pause()
-
-words.forEach(word => {
     let tl = gsap.timeline({
-        repeat: 1,
-        yoyo: true,
-        repeatDelay: 1
-    })
-    tl.to('#text', {
-        duration: 1,
-        text: word
-    })
-    masterTL.add(tl);
+        defaults: {
+            duration: 0.5,
+            ease: 'none',
+            opacity: 0
+        }
+    });
+
+    let tl1 = gsap.timeline({
+        defaults: {
+            duration: 0.1,
+            ease: 'power3.out',
+            opacity: 0,
+            stagger: 0.03
+        }
+    });
+
+    tl1.from(
+        cls3, {
+            x: 100
+        },
+        '+=0.01'
+    );
+
+    tl
+        .from(
+            cls21, {
+                y: -400,
+                ease: 'power3.out'
+            },
+            '+=0.2'
+        )
+        .from(
+            cls26, {
+                x: -400
+            },
+            '-=0.2'
+        )
+        .from(
+            cls27, {
+                x: -400,
+                stagger: 0.2
+            },
+            '+=0.3'
+        )
+        .from(
+            cls22, {
+                y: 100
+            },
+            '-=0.2'
+        )
+        .from(
+            cls23, {
+                x: -400,
+                stagger: 0.2
+            },
+            '-=0.2'
+        );
+
+    tl.from(cls28, {
+        duration: 0.5,
+        stagger: 0.2,
+        transform: 'translateX(-100vw)',
+        ease: 'power3.out',
+        onComplete: () => masterTL.play()
+    });
 });
--- prototypes/ModulAnimationen/MIB8_animation/script.js
+++ prototypes/ModulAnimationen/MIB8_animation/script.js
@@ -1,6 +1,6 @@
 let svgObj = document.getElementById("MIB8Obj");
 
-svgObj.onload = function() {
+svgObj.onload = function () {
     content = svgObj.contentDocument;
 
     let bosTl = animateBos();
@@ -19,7 +19,7 @@ svgObj.onload = function() {
 Rote Punkte blinken
 punkte werden ausgeblendet
 */
-function animateBos(){
+function animateBos() {
     let arrowL = content.getElementById("arrow1");
     let arrowR = content.getElementById("arrow3");
     let red = "#c34745";
@@ -28,47 +28,115 @@ function animateBos(){
     gsap.registerEffect({
         name: "colorBlink",
         effect: (target, config) => {
-            return gsap.to(target, {yoyo: true, repeat: config.repeat, fill: config.fill, opacity: 1, duration: config.duration, repeatDelay: config.repeatDelay});            
+            return gsap.to(target, {
+                yoyo: true,
+                repeat: config.repeat,
+                fill: config.fill,
+                opacity: 1,
+                duration: config.duration,
+                repeatDelay: config.repeatDelay
+            });
         },
-        defaults: {duration: 0.2, repeatDelay: 0.2, repeat: 1},
+        defaults: {
+            duration: 0.2,
+            repeatDelay: 0.2,
+            repeat: 1
+        },
         extendTimeline: true
     });
     gsap.registerEffect({
         name: "sizeBlink",
         effect: (target, config) => {
-            return gsap.to(target, {yoyo: true, repeat: 1, duration: config.duration, scale: config.scale, transformOrigin: "center"});            
+            return gsap.to(target, {
+                yoyo: true,
+                repeat: 1,
+                duration: config.duration,
+                scale: config.scale,
+                transformOrigin: "center"
+            });
         },
-        defaults: {duration: 0.3, scale: 1.3},
+        defaults: {
+            duration: 0.3,
+            scale: 1.3
+        },
         extendTimeline: true
     });
 
-    let tl = gsap.timeline({repeat: -1, repeatDelay: 1});
+    let tl = gsap.timeline({
+        repeat: -1,
+        repeatDelay: 1
+    });
     tl.sizeBlink(arrowL)
-    .colorBlink(getGrid(1,4), {fill: red})
-    .colorBlink(getGrid(1,3), {fill: red})
-    .colorBlink(getGrid(1,2), {fill: red})
-    .colorBlink(getGrid(1,1), {fill: red, repeat:0})
-    .sizeBlink(arrowR)
-    .colorBlink(getGrid(3,4), {fill: blue})
-    .colorBlink(getGrid(3,3), {fill: blue})
-    .colorBlink(getGrid(3,2), {fill: blue})
-    .colorBlink(getGrid(3,1), {fill: blue, repeat:0})
-    .sizeBlink(arrowL)
-    .colorBlink(getGrid(1,4), {fill: red})
-    .colorBlink(getGrid(1,3), {fill: red})
-    .colorBlink(getGrid(1,2), {fill: red, repeat:0})
-    .sizeBlink(arrowR)
-    .colorBlink(getGrid(3,4), {fill: blue})
-    .colorBlink(getGrid(3,3), {fill: blue})
-    .colorBlink(getGrid(3,2), {fill: blue, repeat:0})
-    .sizeBlink(arrowL)
-    .colorBlink(getGrid(1,4), {fill: red})
-    .colorBlink(getGrid(1,3), {fill: red, repeat:0})
-    .sizeBlink(arrowR)
-    .colorBlink(getGrid(3,4), {fill: blue})
-    .colorBlink(getGrid(3,3), {fill: blue, repeat:0})
-    .sizeBlink(arrowL)
-    .colorBlink(getGrid(1,4), {fill: red, repeat:0});
+        .colorBlink(getGrid(1, 4), {
+            fill: red
+        })
+        .colorBlink(getGrid(1, 3), {
+            fill: red
+        })
+        .colorBlink(getGrid(1, 2), {
+            fill: red
+        })
+        .colorBlink(getGrid(1, 1), {
+            fill: red,
+            repeat: 0
+        })
+        .sizeBlink(arrowR)
+        .colorBlink(getGrid(3, 4), {
+            fill: blue
+        })
+        .colorBlink(getGrid(3, 3), {
+            fill: blue
+        })
+        .colorBlink(getGrid(3, 2), {
+            fill: blue
+        })
+        .colorBlink(getGrid(3, 1), {
+            fill: blue,
+            repeat: 0
+        })
+        .sizeBlink(arrowL)
+        .colorBlink(getGrid(1, 4), {
+            fill: red
+        })
+        .colorBlink(getGrid(1, 3), {
+            fill: red
+        })
+        .colorBlink(getGrid(1, 2), {
+            fill: red,
+            repeat: 0
+        })
+        .sizeBlink(arrowR)
+        .colorBlink(getGrid(3, 4), {
+            fill: blue
+        })
+        .colorBlink(getGrid(3, 3), {
+            fill: blue
+        })
+        .colorBlink(getGrid(3, 2), {
+            fill: blue,
+            repeat: 0
+        })
+        .sizeBlink(arrowL)
+        .colorBlink(getGrid(1, 4), {
+            fill: red
+        })
+        .colorBlink(getGrid(1, 3), {
+            fill: red,
+            repeat: 0
+        })
+        .sizeBlink(arrowR)
+        .colorBlink(getGrid(3, 4), {
+            fill: blue
+        })
+        .colorBlink(getGrid(3, 3), {
+            fill: blue,
+            repeat: 0
+        })
+        .sizeBlink(arrowL)
+        .colorBlink(getGrid(1, 4), {
+            fill: red,
+            repeat: 0
+        });
 }
 
 /*IDE
@@ -82,7 +150,7 @@ View wird ausgewählt
 
 */
 
-function animateIDE(){
+function animateIDE() {
     let viewTab = content.getElementById("ViewTab");
     let modelTab = content.getElementById("ModelTab");
     let controllerTab = content.getElementById("ControllerTab");
@@ -93,7 +161,9 @@ function animateIDE(){
     let selectColor = "#4a465d";
     let defaultColor = "#1d1835";
 
-    let master = gsap.timeline({repeat: -1});
+    let master = gsap.timeline({
+        repeat: -1
+    });
     let viewTl = gsap.timeline();
     let modelTl = gsap.timeline();
     let controllerTl = gsap.timeline();
@@ -102,55 +172,118 @@ function animateIDE(){
         name: "scaleIncrementX",
         effect: (target, config) => {
             let iter = Math.abs((1 - config.scale) / config.increment);
-            if(1 > config.scale){
+            if (1 > config.scale) {
                 config.increment *= -1;
             }
 
             let tl = gsap.timeline();
-            for(let i=0;i<iter;i++){
-                tl.to(target, {duration: config.duration, scaleX: "+=" + config.increment, delay: config.delay});
+            for (let i = 0; i < iter; i++) {
+                tl.to(target, {
+                    duration: config.duration,
+                    scaleX: "+=" + config.increment,
+                    delay: config.delay
+                });
             }
 
             return tl;
         },
-        defaults: {increment: 0.05, delay:0.05, duration: 0.02},
+        defaults: {
+            increment: 0.05,
+            delay: 0.05,
+            duration: 0.02
+        },
         extendTimeline: true
     });
 
-    viewTl.to(viewCode, {opacity: 1})
-    .to(viewTab, {fill: selectColor}, "<")
-    .scaleIncrementX(viewCode.children[0], {scale: 0.5})
-    .scaleIncrementX(viewCode.children[7], {scale: 1.2}, ">0.5")
-    .scaleIncrementX(viewCode.children[3], {scale: 0, delay:0.03}, ">0.5")
-    .scaleIncrementX(viewCode.children[13], {scale: 1.4}, ">0.5")
-    .to(viewCode, {opacity: 0})
-    .to(viewTab, {fill: defaultColor}, "<");
+    viewTl.to(viewCode, {
+            opacity: 1
+        })
+        .to(viewTab, {
+            fill: selectColor
+        }, "<")
+        .scaleIncrementX(viewCode.children[0], {
+            scale: 0.5
+        })
+        .scaleIncrementX(viewCode.children[7], {
+            scale: 1.2
+        }, ">0.5")
+        .scaleIncrementX(viewCode.children[3], {
+            scale: 0,
+            delay: 0.03
+        }, ">0.5")
+        .scaleIncrementX(viewCode.children[13], {
+            scale: 1.4
+        }, ">0.5")
+        .to(viewCode, {
+            opacity: 0
+        })
+        .to(viewTab, {
+            fill: defaultColor
+        }, "<");
 
-    modelTl.to(modelCode, {opacity: 1})
-    .to(modelTab, {fill: selectColor}, "<")
-    .scaleIncrementX(modelCode.children[5], {scale: 0.5})
-    .scaleIncrementX(modelCode.children[12], {scale: 1.2}, ">0.5")
-    .scaleIncrementX(modelCode.children[2], {scale: 0, delay:0.03}, ">0.5")
-    .scaleIncrementX(modelCode.children[7], {scale: 1.4}, ">0.5")
-    .scaleIncrementX(modelCode.children[10], {scale: 1.4}, ">0.5")
-    .to(modelCode, {opacity: 0})
-    .to(modelTab, {fill: defaultColor}, "<");
+    modelTl.to(modelCode, {
+            opacity: 1
+        })
+        .to(modelTab, {
+            fill: selectColor
+        }, "<")
+        .scaleIncrementX(modelCode.children[5], {
+            scale: 0.5
+        })
+        .scaleIncrementX(modelCode.children[12], {
+            scale: 1.2
+        }, ">0.5")
+        .scaleIncrementX(modelCode.children[2], {
+            scale: 0,
+            delay: 0.03
+        }, ">0.5")
+        .scaleIncrementX(modelCode.children[7], {
+            scale: 1.4
+        }, ">0.5")
+        .scaleIncrementX(modelCode.children[10], {
+            scale: 1.4
+        }, ">0.5")
+        .to(modelCode, {
+            opacity: 0
+        })
+        .to(modelTab, {
+            fill: defaultColor
+        }, "<");
 
-    controllerTl.to(controllerCode, {opacity: 1})
-    .to(controllerTab, {fill: selectColor}, "<")
-    .scaleIncrementX(controllerCode.children[0], {scale: 0})
-    .scaleIncrementX(controllerCode.children[1], {scale: 0}, ">0.5")
-    .scaleIncrementX(controllerCode.children[2], {scale: 0, delay:0.03}, ">0.5")
-    .scaleIncrementX(controllerCode.children[0], {scale: 2}, ">0.5")
-    .scaleIncrementX(controllerCode.children[1], {scale: 2.5}, ">0.5")
-    .to(controllerCode, {opacity: 0})
-    .to(controllerTab, {fill: defaultColor}, "<");
+    controllerTl.to(controllerCode, {
+            opacity: 1
+        })
+        .to(controllerTab, {
+            fill: selectColor
+        }, "<")
+        .scaleIncrementX(controllerCode.children[0], {
+            scale: 0
+        })
+        .scaleIncrementX(controllerCode.children[1], {
+            scale: 0
+        }, ">0.5")
+        .scaleIncrementX(controllerCode.children[2], {
+            scale: 0,
+            delay: 0.03
+        }, ">0.5")
+        .scaleIncrementX(controllerCode.children[0], {
+            scale: 2
+        }, ">0.5")
+        .scaleIncrementX(controllerCode.children[1], {
+            scale: 2.5
+        }, ">0.5")
+        .to(controllerCode, {
+            opacity: 0
+        })
+        .to(controllerTab, {
+            fill: defaultColor
+        }, "<");
 
     master.add(viewTl);
     master.add(modelTl);
     master.add(controllerTl);
 }
 
-function getGrid(x, y){
+function getGrid(x, y) {
     return content.getElementById(x + "" + y);
 }
