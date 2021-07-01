ce55819985c21bc548e9e7241251d9fe273d447b
Marc Rosenberger <marc.rosenberger@iem.thm.de>
2021-07-01 13:19:09 +0200

    UPDATE: Added Mib3 Animation

--- assets/js/modulAnimInserter.js
+++ assets/js/modulAnimInserter.js
@@ -32,7 +32,7 @@ function loadAnimationScript(moduleId, content){
             mib2Anim(content);
             break;
         case "MIB3" :
-            //mib3Anim(content); //animation script doesnt work with object tag
+            mib3Anim(content);
             break;
         case "MIB4a" :
             mib4Anim(content);
@@ -179,15 +179,52 @@ function mib2Anim(content) {
 }
 
 function mib3Anim(content) {
-    var tl = gsap.timeline({
+    let cls3 = content.querySelectorAll('.cls-3, .cls-18');
+    let cls21 = content.getElementsByClassName('cls-21');
+    let cls22 = content.getElementsByClassName('cls-22');
+    let cls23 = content.getElementsByClassName('cls-23');
+    let cls26 = content.getElementsByClassName('cls-26');
+    let cls27 = content.getElementsByClassName('cls-27');
+    let cls28 = content.getElementsByClassName('cls-28');
+    let cursor = content.getElementById('cursor');
+    let title = content.querySelectorAll('.title');
+
+    let c = gsap.to(cursor, {
+        opacity: 0,
+        ease: 'power2.inOut',
+        repeat: -1
+    });
+
+    const words = ['Designer.', 'Entwickler.', 'Entrepreneur.'];
+
+    let masterTL = gsap
+        .timeline({
+            repeat: -1
+        })
+        .pause();
+
+    words.forEach((word) => {
+        let tl = gsap.timeline({
+            repeat: 1,
+            yoyo: true,
+            repeatDelay: 1
+        });
+        tl.to(title, {
+            duration: 1,
+            text: word
+        });
+        masterTL.add(tl);
+    });
+
+    let tl = gsap.timeline({
         defaults: {
             duration: 0.5,
             ease: 'none',
             opacity: 0
         }
     });
-    
-    var tl1 = gsap.timeline({
+
+    let tl1 = gsap.timeline({
         defaults: {
             duration: 0.1,
             ease: 'power3.out',
@@ -195,59 +232,55 @@ function mib3Anim(content) {
             stagger: 0.03
         }
     });
-    
-    tl1.from(content.getElementById('cls3'), {
-        x: 100,
-    }, '+=0.01')
 
-    tl.from(content.getElementsByClassName('cls-21'), {
-        y: -400,
-        ease: 'power3.out'
-    }, '+=0.2').from(content.getElementsByClassName('cls-26'), {
-        x: -400
-    }, '-=0.2').from(content.getElementsByClassName('cls-27'), {
-        x: -400,
-        stagger: 0.2
-    }, '+=0.3').from(content.getElementsByClassName('cls-22'), {
-        y: 100
-    }, '-=0.2').from(content.getElementsByClassName('cls-23'), {
-        x: -400,
-        stagger: 0.2
-    }, '-=0.2');
-    
-    
-    tl.from(content.getElementsByClassName('cls-28'), {
+    tl1.from(
+        cls3, {
+            x: 100
+        },
+        '+=0.01'
+    );
+
+    tl
+        .from(
+            cls21, {
+                y: -400,
+                ease: 'power3.out'
+            },
+            '+=0.2'
+        )
+        .from(
+            cls26, {
+                x: -400
+            },
+            '-=0.2'
+        )
+        .from(
+            cls27, {
+                x: -400,
+                stagger: 0.2
+            },
+            '+=0.3'
+        )
+        .from(
+            cls22, {
+                y: 100
+            },
+            '-=0.2'
+        )
+        .from(
+            cls23, {
+                x: -400,
+                stagger: 0.2
+            },
+            '-=0.2'
+        );
+
+    tl.from(cls28, {
         duration: 0.5,
         stagger: 0.2,
-        x: "-100vw",
-        ease: "power3.out",
+        transform: 'translateX(-100vw)',
+        ease: 'power3.out',
         onComplete: () => masterTL.play()
-    });
-    
-    const words = ['Designer.', 'Entwickler.', 'Entrepreneur.'];
-    
-    let cursor = gsap.to(content.getElementById('cursor'), {
-        opacity: 0,
-        ease: 'power2.inOut',
-        repeat: -1
-    });
-    
-    let masterTL = gsap.timeline({
-        repeat: -1,
-    
-    }).pause();
-    
-    words.forEach(word => {
-        let tl = gsap.timeline({
-            repeat: 1,
-            yoyo: true,
-            repeatDelay: 1
-        })
-        tl.to(content.getElementById('text'), {
-            duration: 1,
-            text: word
-        })
-        masterTL.add(tl);
     });
 }
 
